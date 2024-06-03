6f740e687cbf0abd8eb057bdd6e5e4c42f42ea05
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2024-06-04 01:25:09 +0800

    Add files via upload

---
+++ UI/My_Account.js
@@ -0,0 +1,29 @@
+function updateDateTime() {
+    const now = new Date();
+    const options = { 
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric',
+        hour: '2-digit', minute: '2-digit', second: '2-digit'
+    };
+    const formattedDateTime = now.toLocaleDateString('en-US', options);
+    document.getElementById('datetime').innerHTML = formattedDateTime;
+}
+
+setInterval(updateDateTime, 1000);
+updateDateTime();
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the default tab (History)
+document.getElementsByClassName("tab-button")[0].click();
