c3a03dfc36a7525f3160ec6527cf70b852c81f0b
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2024-07-09 03:43:37 +0800

    Add files via upload

--- public/js/Founders.js
+++ public/js/Founders.js
@@ -48,3 +48,57 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const descriptionContainers = document.querySelectorAll('.founder-description-container');
+
+    function checkOverflow() {
+        descriptionContainers.forEach(container => {
+            const description = container.querySelector('.founder-description');
+            const showMore = container.querySelector('.show-more');
+            const isExpanded = description.style.maxHeight === 'none';
+            
+            if (description.scrollHeight > description.clientHeight || isExpanded) {
+                showMore.style.display = 'block';
+            } else {
+                showMore.style.display = 'none';
+            }
+        });
+    }
+
+    checkOverflow();
+
+    window.addEventListener('resize', checkOverflow);
+});
+
+function toggleDescription(element) {
+    const description = element.previousElementSibling;
+    const isExpanded = description.style.maxHeight === 'none';
+    
+    if (isExpanded) {
+        description.style.maxHeight = description.scrollHeight + 'px';
+        setTimeout(() => {
+            description.style.maxHeight = '200px';
+        }, 10);
+        element.innerHTML = '&#x25B2;';
+        element.classList.add('rotate');
+    } else {
+        description.style.maxHeight = description.scrollHeight + 'px';
+        setTimeout(() => {
+            description.style.maxHeight = 'none';
+        }, 500); // This should match the transition duration
+        element.innerHTML = '&#x25BC;';
+        element.classList.add('rotate');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
