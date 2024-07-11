53b302bec79640b69814bf1b30a29c67f554bca5
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2024-07-11 22:11:11 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(session({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(routes);
 initData();
 
