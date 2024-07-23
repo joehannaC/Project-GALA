5cd7f22e939869d69fdc51df68fc4dad921dd47e
AndreiOViguilla <140049239+AndreiOViguilla@users.noreply.github.com>
2024-07-23 18:33:21 +0800

    Add files via upload

--- public/js/donate-popup.js
+++ public/js/donate-popup.js
@@ -9,9 +9,11 @@ function closeDonateModal() {
 function showImage(type) {
     var donationImage = document.getElementById('donationImage');
     if (type === 'bank') {
-        donationImage.src = 'images/Donation/Payment Method/Bank.jpg'; 
+        donationImage.src = 'images/Donation/Payment Method/Bank.jpg';
+        donationImage.className = 'donation-image-bank';
     } else if (type === 'gcash') {
-        donationImage.src = 'images/Donation/Payment Method/Gcash.jpg'; 
+        donationImage.src = 'images/Donation/Payment Method/Gcash.jpg';
+        donationImage.className = 'donation-image-gcash'; 
     }
     donationImage.style.display = 'block';
 }
