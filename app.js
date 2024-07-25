ae83a36a1ef743ee232fec2b599528a8c1951858
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2024-07-25 17:09:27 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(routes);
 initData();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
