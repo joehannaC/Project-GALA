b1e5f3c0c2125f9bcdbb2d1f0a6b35b7bd311ae6
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2024-08-02 11:14:38 +0800

    Update mailer.js

--- src/mailer.js
+++ src/mailer.js
@@ -3,11 +3,14 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.zoho.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
