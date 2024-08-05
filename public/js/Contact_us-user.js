1b4eef459af8f4d8a58f3c8485dc4b45da625020
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2024-08-05 15:44:38 +0800

    Update Contact_us-user.js

--- public/js/Contact_us-user.js
+++ public/js/Contact_us-user.js
@@ -38,4 +38,51 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     updateNavigation();
+    loadContact();
 });
+
+function convertTo12HourFormat(time) {
+    let [hour, minute] = time.split(':');
+    let period = 'AM';
+
+    hour = parseInt(hour);
+    if (hour >= 12) {
+        period = 'PM';
+        if (hour > 12) hour -= 12;
+    } else if (hour === 0) {
+        hour = 12;
+    }
+    return `${hour}:${minute} ${period}`;
+}
+
+function loadContact() {
+    fetch('/getContact')
+        .then(response => response.json())
+        .then(data => {
+            if (data.contact) {
+                document.getElementById('address').textContent= data.contact.Address;
+                document.getElementById('email-address').textContent = data.contact.Email;
+                const phoneElement = document.getElementById('contact-number');
+                const phone = data.contact.Phone;
+                const network = data.contact.Network.charAt(0).toUpperCase() + data.contact.Network.slice(1);
+                const phoneText = `${network}: ${phone}`;
+                const p = document.createElement('p');
+                p.textContent = phoneText;
+                phoneElement.appendChild(p);
+                const businessHoursElement = document.getElementById('business-hours');
+                const businessHours = data.businessHours;
+                businessHours.forEach(businessHour => {
+                    const day = businessHour.Day.charAt(0).toUpperCase() + businessHour.Day.slice(1);
+                    const startTime = convertTo12HourFormat(businessHour.StartTime.substring(0, 5));
+                    const endTime = convertTo12HourFormat(businessHour.EndTime.substring(0, 5));
+                    const businessHourText = `${day}: ${startTime} - ${endTime}`;
+                    const p = document.createElement('p');
+                    p.textContent = businessHourText;
+                    businessHoursElement.appendChild(p);
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching contact:', error);
+        });
+}
